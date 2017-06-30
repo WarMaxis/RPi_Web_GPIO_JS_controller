1e15adf66c400b4beb6a8ab8cf1d19c0df7f0570
Tomasz Szarek <tomasz.szarek@sollers.eu>
2017-07-01 01:32:57 +0200

    Ver 1.0 Basic Mock Server Basic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { requestOptionsProvider }   from './default-request-options.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PinController } from './controller/controller.component';
+import { PinComponent } from './controller/pin/pin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PinController,
+    PinComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ requestOptionsProvider ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
