a4f12f5e2fd97bb308317c5be27935784de8787f
Tomasz Szarek <tomasz93szarek@gmail.com>
2017-07-10 11:12:52 +0200

    Server update. Adding array of Pins.

--- server/serverMock.js
+++ server/serverMock.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
-
+var pinArray = [];
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -12,13 +12,15 @@ app.use(function(req, res, next) {
 
 app.post('/pin/set', function (req, res) {
   var pinData = req.body;
+  pinArray[pinData.ID] = pinData;
   if (pinData.enabled) {
     if (!pinData.input) {
       console.log(pinData);
       res.send("Pin enabled and output set");
+    }
   } else {
     console.log(pinData);
-    res.send("Pin disabled")
+    res.send("Pin disabled");
   }
 });
 
@@ -31,18 +33,19 @@ app.get('/pin/get', function (req, res) {
 });
 
 app.get('/all/get', function (req, res) {
- var pinArray = []
-  for (var i = 0; i<7 ; i++) {
-    pinArray.push({
-      ID: i,
-      value: 0,
-      enabled: false,
-      input: true,
-      frequency: 50
-    });
+  if (pinArray.length == 0) {
+    for (var i = 0; i<8 ; i++) {
+      pinArray.push({
+        ID: i,
+        value: 0,
+        enabled: false,
+        input: true,
+        frequency: 50
+      });
+    }
   }
   var response = JSON.stringify(pinArray);
-   console.log("Pin Array Send");
+   console.log(pinArray);
    res.send(response);
 });
 
--- server/serverRasp.js
+++ server/serverRasp.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 const raspi = require('raspi');
 const gpio = require('raspi-gpio');
 const softPwm = require('raspi-soft-pwm');
+var pinArray = [];
 
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
@@ -15,6 +16,8 @@ app.use(function(req, res, next) {
 
 app.post('/pin/set', function (req, res) {
   var pinData = req.body;
+  var pinData = req.body;
+  pinArray[pinData.ID] = pinData;
   if (pinData.enabled) {
     if (!pinData.input) {
       raspi.init(() => {
@@ -47,18 +50,19 @@ app.get('/pin/get', function (req, res) {
 });
 
 app.get('/all/get', function (req, res) {
- var pinArray = []
-  for (var i = 0; i<7 ; i++) {
-    pinArray.push({
-      ID: i,
-      value: 0,
-      enabled: false,
-      input: true,
-      frequency: 50
-    });
+  if (pinArray.length == 0) {
+    for (var i = 0; i<8 ; i++) {
+      pinArray.push({
+        ID: i,
+        value: 0,
+        enabled: false,
+        input: true,
+        frequency: 50
+      });
+    }
   }
   var response = JSON.stringify(pinArray);
-   console.log("Pin Array Send");
+   console.log(pinArray);
    res.send(response);
 });
 
